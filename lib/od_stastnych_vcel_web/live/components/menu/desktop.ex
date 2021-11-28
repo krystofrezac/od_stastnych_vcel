aa4a1c15e91e8a2534ad2de64118080bcb04ecd3
Kryštof Řezáč <krystofrezac@gmail.com>
2021-11-28 10:13:37 +0100

    Add logo to nav

--- lib/od_stastnych_vcel_web/live/components/menu/desktop.ex
+++ lib/od_stastnych_vcel_web/live/components/menu/desktop.ex
@@ -1,5 +1,5 @@
 defmodule OdStastnychVcelWeb.Live.Components.Menu.Desktop do
-  use Phoenix.Component
+  use OdStastnychVcelWeb, :component
 
   @spec main(%{
           socket: %Phoenix.LiveView.Socket{},
@@ -10,15 +10,24 @@ defmodule OdStastnychVcelWeb.Live.Components.Menu.Desktop do
   def main(assigns) do
     ~H"""
     <div class={@class <> " sticky top-0 -mb-2 w-full z-10"}>
-      <div class="bg-black bg-opacity-90 p-5 z-20 relative">
-        <div class="container m-auto flex justify-end">
-          <%= for {title, link} <- @menu_items do %>
-            <%= live_redirect to: link do %>
-              <span class="text-xl pl-4 text-white">
-                <%= title %>
-              </span>
+      <div class="bg-black bg-opacity-90 px-5 py-3 z-20 relative">
+        <div class="container flex align-center mx-auto">
+          <img 
+            class="w-8 h-8"
+            src={Routes.static_path(@socket, "/images/logo.svg")}
+          />
+          <span class="text-white text-2xl pl-5 my-auto">
+            Od štastných včel 
+          </span>
+          <div class="flex justify-end ml-auto items-center">
+            <%= for {title, link} <- @menu_items do %>
+              <%= live_redirect to: link do %>
+                <span class="text-xl pl-4 text-white">
+                  <%= title %>
+                </span>
+              <% end %>
             <% end %>
-          <% end %>
+          </div>
         </div>
       </div>
       <div class="h-2 w-full" style="background: rgb(0,0,0); background: linear-gradient(0deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.3) 100%);">
