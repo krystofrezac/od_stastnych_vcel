353b17e5d637649bad4c65fd50cd7ee190b0f22f
Kryštof Řezáč <krystofrezac@gmail.com>
2021-12-13 17:02:26 +0100

    Add dialyzer

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule OdStastnychVcel.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 
