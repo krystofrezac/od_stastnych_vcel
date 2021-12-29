829bc355d1123681e74fd1f87f075ef341868fdb
Kryštof Řezáč <krystofrezac@gmail.com>
2021-12-29 09:48:20 +0100

    Move template from app to live

--- lib/bees_web/live/admin/user_auth_hook.ex
+++ lib/bees_web/live/admin/user_auth_hook.ex
@@ -9,20 +9,31 @@ defmodule BeesWeb.Admin.Live.UserAuthHook do
   # credo:disable-for-next-line Credo.Check.Readability.AliasAs
   alias BeesWeb.Router.Helpers, as: Routes
 
-  @spec on_mount(atom(), map(), %{optional(:user_token) => String.t()}, LiveView.Socket) ::
-          {:cont, LiveView.Socket} | {:halt, LiveView.Socket}
+  @spec on_mount(atom(), map(), %{optional(:user_token) => String.t()}, LiveView.Socket.t()) ::
+          {:cont, LiveView.Socket.t()} | {:halt, LiveView.Socket.t()}
   def on_mount(_type, _opts, session, socket) do
     user_token = Map.get(session, "user_token")
 
     with true <- user_token != nil,
-         true <- Accounts.get_user_by_session_token(user_token) != nil do
-      {:cont, socket}
+         {:ok, user} <- get_user(user_token) do
+      {:cont,
+       socket
+       |> LiveView.assign(:current_user, user)}
     else
       _error ->
         {:halt,
          socket
          |> LiveView.put_flash(:error, "Musíte se nejdříve přihlásit")
          |> LiveView.redirect(to: Routes.user_session_path(socket, :new))}
+    end
+  end
+
+  defp get_user(token) do
+    user = Accounts.get_user_by_session_token(token)
+
+    case user do
+      nil -> {:error, nil}
+      u -> {:ok, u}
     end
   end
 end
