8a1d105284190d404ecd8847bc12ae5781a7ef96
Kryštof Řezáč <krystofrezac@gmail.com>
2022-01-09 14:59:31 +0100

    Configure fly database

--- config/runtime.exs
+++ config/runtime.exs
@@ -15,8 +15,8 @@ if config_env() == :prod do
       """
 
   config :bees, Bees.Repo,
-    # ssl: true,
-    # socket_options: [:inet6],
+    ssl: false,
+    socket_options: [:inet6],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
